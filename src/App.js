f90b89bac8b5631d67c9763b47d4aa96e6b5d692
alantollett <alantollett@outlook.com>
2021-03-02 16:30:55 +0000

    Added code to get stock data from back-end api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            error: null,
+            portfolios: null
+        };
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/').then(res => {
+            this.setState({portfolios: res.data});
+        }).catch(err => {
+            this.setState({error: err});
+        });
     }
 
     render = () => {
-        return (
-            <h1>Hello, World!</h1>
-        );
+        const {error, portfolios} = this.state;
+
+        if(error){
+            return <div>{error.message}</div>
+        }else if(!portfolios){
+            return <div>Downloading Stock Data...</div>
+        }else{
+            return (
+                <ul>
+                    {portfolios.map((portfolio, index) => 
+                        <li key={index}>
+                            {portfolio.weights} {portfolio.expectedReturn} {portfolio.standardDeviation}
+                        </li>)}
+                </ul>
+            );
+        }
     }
 }

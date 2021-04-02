3742836386adad143f47233b87c19bc1d5bc2494
alantollett <alantollett@outlook.com>
2021-04-02 16:42:47 +0100

    Added hover text to the efficient frontier visualisation

--- src/components/pages/Dashboard/Graph.js
+++ src/components/pages/Dashboard/Graph.js
@@ -16,18 +16,35 @@ export default class Graph extends React.Component {
         }else if(!portfolios){
             return <div>Downloading Stock Data...</div>
         }else{
+            var hoverTexts = [];
+            portfolios.forEach(portfolio => hoverTexts.push(portfolio.asString));
+
             return (
                 <Plot className="graph"
                     data={[
                         {
                             x: standardDeviations,
                             y: expectedReturns,
+                            text: hoverTexts,
                             type: 'scatter',
                             mode: 'markers',
                             marker: {color: 'red'},
+                            hovertemplate: "<b>Expected Return: %{y}%</b><br>"
+                                + "<b>Standard Deviation: %{x}%</b><br>"
+                                + "%{text}"
+                                + "<extra></extra>"
                         }
                     ]}
-                    layout={ {width: 500, height: 500, title: 'Efficient Frontier'} }
+                    layout={{
+                        // width: 500, 
+                        // height: 500, 
+                        title: 'Efficient Frontier',
+                        xaxis: { title: "Standard Deviation (%)" },
+                        yaxis: { title: "Expected Return (%)" },        
+                        hovermode: 'closest',
+                        hoverlabel: { bgcolor: "#FFF" },
+                        config: { responsive: true }
+                    }}
                 />
             );
         }
